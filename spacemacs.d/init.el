0821c073538a2da90f50c1abb5c9c7ecd1dce6bf
baoxianzhang <baoxianzhit@gmail.com>
2016-08-14 10:28:47 +0800

    add games and ranger to the init.el.
    gtags and cscope don't work right, need more try.

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -48,7 +48,12 @@ values."
      (colors :variables
              colors-enable-nyan-cat-progress-bar t)
      version-control
-     gtags
+     ;; gtags
+     ;; cscope
+     ;; erc
+     games
+     ranger
+     ;; fasd
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -287,10 +292,7 @@ you should place your code here."
             :empty-lines 1)
            ))
    (setq org-agenda-files (list "~/org/TODO.org"
-                                "~/org/gtd.org"
-                                "~/org/work.org"
-                                "~/org/life.org"
-                                "~/org/self.org"))
+                                "~/org/gtd.org"))
 
    ;; (define-key global-map (kbd "C-c y") 'youdao-dictionary-search-at-point+)
    (spacemacs/set-leader-keys "o y" 'youdao-dictionary-search-at-point+)
@@ -323,16 +325,3 @@ you should place your code here."
 ;; (load custom-file 'no-error 'no-message)
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-tooltip-common ((t (:inherit company-tooltip :weight bold :underline nil))))
- '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :weight bold :underline nil)))))
