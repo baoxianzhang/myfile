839f81b48fa21262b6baaddcd995fcc9b40027ed
baoxianzhang <baoxianzhit@gmail.com>
2019-02-02 19:50:24 +0800

    feat: add clone code script

---
+++ clone_code.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env sh
+
+mkdir -p ~/code
+cd ~/code
+git clone https://github.com/CPFL/Autoware.git
+git clone https://github.com/googlecartographer/cartographer.git
+git clone https://github.com/googlecartographer/cartographer_ros.git
+git clone https://github.com/artsy/eigen.git
+git clone https://github.com/ethz-asl/ethzasl_icp_mapping.git
+git clone https://github.com/ethz-asl/ethzasl_msf.git
+git clone https://github.com/devbharat/gtsam.git
+git clone https://github.com/dongjing3309/gtsam-examples.git
+git clone https://github.com/ethz-asl/libnabo.git
+git clone https://github.com/ethz-asl/libpointmatcher.git
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/raulmur/ORB_SLAM2.git
+git clone https://github.com/PointCloudLibrary/pcl.git
+git clone https://github.com/HKUST-Aerial-Robotics/VINS-Mono.git
+git clone https://github.com/ros/ros_comm.git
